ef1e6323489fe36b9080fef90eb528d6cd130986
Vojtech Kucera <kucerao@outlook.cz>
2023-03-27 15:51:39 +0200

    Added introduction with inserting username, password and choice of the analyzed text to task_templated

---
+++ task_template.py
@@ -0,0 +1,72 @@
+'''
+projekt_1.py: první projekt do Engeto Online Python Akademie
+Textovy_analyzator
+
+author: Vojtech Kucera
+email: vojtechkuc@gmail.com
+discord: Vojta K.
+'''
+
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+
+# registered users
+########################################################
+users = {
+    'bob': '123',
+    'ann': 'pass123',
+    'mike': 'password123',
+    'liz': 'pass123'
+}
+
+separator = 40 * '-'
+# input of nickname and password
+#######################################################
+username = input('Insert your username: ')
+password = input('Insert your password: ')
+
+
+# authentication of registered user and valid password
+if username in users:
+
+    if users[username] == password:
+        print(separator,
+              f'Welcome to the app, {username}',
+              'We have 3 texts to be analyzed.',
+              separator,
+              sep='\n')
+    else:
+        print('password not valid')
+        quit()
+else:
+    print('Unregistered user, terminating the program...')
+    quit()
+
+# choice of text to be analyzed
+#######################################################
+text_choice = int(input('Enter a number btw. 1 and 3 to select: '))
